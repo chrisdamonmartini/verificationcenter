bf022667fe4cf487163d6ae3833ac3cbc5b30dee
Chris Martini <chris.martini@siemens.com>
2025-03-22 10:06:01 -0500

    Update common component exports

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -2,6 +2,7 @@ import DetailExample from './DetailExample';
 import DetailTable from './DetailTable';
 import RelatedItemsPanel from './RelatedItemsPanel';
 import ContentPanel from './ContentPanel';
+import ContentPanelExample from './ContentPanelExample';
 
-export { DetailExample, DetailTable, RelatedItemsPanel, ContentPanel };
+export { DetailExample, DetailTable, RelatedItemsPanel, ContentPanel, ContentPanelExample };
 export default RelatedItemsPanel; 
